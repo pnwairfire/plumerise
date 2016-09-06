92308e383af024782f163d5e1c039a6c7630b127
Joel Dubowy <jdubowy@gmail.com>
2016-09-06 09:04:52 -0700

    Make sure pyairfire <2.0.0
    major version changes introduce backwards incompatible changesu

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     url='https://github.com/pnwairfire/plumerise',
     description='Package for computing plume rise from time-profiled emissions output.',
     install_requires=[
-        "pyairfire>=1.1.1"
+        "pyairfire>=1.1.1,<2.0.0"
     ],
     dependency_links=[
         "https://pypi.smoke.airfire.org/simple/pyairfire/",
